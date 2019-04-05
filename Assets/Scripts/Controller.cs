33bc2bbc908bcfd8d5c0d4b693ff951ca4bbff3d
DESKTOP-85C8DUS\KaliBird <ky3971@yandex.ru>
2019-04-05 19:57:37 +0300

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    //[SerializeField]
+    //private Transform space;
+    ////var object1 = Instantiate(Resources.Load("SpaceObjects/Object1", typeof(GameObject)), space) as GameObject;
+
+
+    public const float step_time = 0.02f;
+
+    public float G;
+    [SerializeField]
+    private SpaceObject[] spaceObjects;
+
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        InvokeRepeating("Step", 0, step_time);   
+    }
+
+    private void Step()
+    {
+        foreach (var objectA in spaceObjects)
+        {
+            foreach (var objectB in spaceObjects)
+            {
+                if (objectB == objectA) continue;
+
+                Vector3 d_rad_A = objectB.transform.position - objectA.transform.position;//Радиус вектор для объект_А в точку объект_B
+                float pow_2_rad = Mathf.Pow(d_rad_A.magnitude, 2);//квадрат расстояния между точками
+
+                if (pow_2_rad <= 0.00001) pow_2_rad = 0.00001f;
+                //Упростить расчетом Vector3 =  (G*d_rad_A) / pow_2_rad;
+
+                objectA.Acceleration = (G*objectB.mass / pow_2_rad) * d_rad_A.normalized;//Вектор ускорения [a] = (G*mass другого объекта / радиус квадрат)* единичное направление вектора
+                //objectB.Acceleration = -1 * (G*objectA.mass / pow_2_rad+0.01f) * d_rad_A.normalized;//Вектор ускорение [a] другого объекта (-1 определяет противоположное направление радиус вектора
+            }
+        }
+
+        foreach (var obj in spaceObjects)
+            obj.Positionize();
+    }
+}
---
+++ Assets/Scripts/SpaceObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceObject : MonoBehaviour
+{
+    public const float G = 1; // 6.67 * 10^(-11) [(Н * м^2) / (кг^2)]
+
+    [SerializeField]
+    public float mass; //[кг]
+
+    [Header("Physics components")]
+    public Vector3 v;//[м/с]
+    [HideInInspector]
+    public Vector3 a;//[м^2/c]
+
+
+    [HideInInspector]
+    public float Gm;
+
+
+    private void Awake()
+    {
+        Gm = G * mass;
+    }
+
+    /// <summary>
+    /// при установке сразу изменяет вектор скорости (предопеределен Controller.step_time)
+    /// </summary>
+    public Vector3 Acceleration
+    {
+        get
+        {
+            return a;
+        }
+        set
+        {
+            a = value;
+            v += a * Controller.step_time;
+        }
+    }
+
+    /// <summary>
+    /// Устанавливает координату (предопеределен Controller.step_time)
+    /// </summary>
+    public void Positionize()
+    {
+        transform.position += v * Controller.step_time;
+    }
+
+}
